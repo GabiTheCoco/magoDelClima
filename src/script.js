fa908c470197d7ff332e752c83638bcdec9e073a
Gonza Bianchi <gonzalobianchini70@gmail.com>
2023-10-10 11:27:32 -0300

    v0.0.3

--- src/script.js
+++ src/script.js
@@ -1,59 +1,89 @@
-
-let input_country = document.querySelector(".main_container_form_inputs-country");
+import { createLI } from "./components/countries.js";
+let input_country = document.querySelector("[input_country]");
 let label_country = document.querySelector(".label_country");
 let form_block = document.querySelector("[main_container-form]");
 let header_section = document.querySelector("[main_container]");
 let main = document.querySelector("[main_content]");
+let countryUl = document.querySelector(".main_container_form_inputs-ul");
 
 let weather_map_section = document.createElement("section");
 let weather_container = document.createElement("div");
 let map_container = document.createElement("div");
 
-
-
-let change = JSON.parse(localStorage.getItem("change"));
-
-changePresentation(change);
-createWeatherMapSection();
-
-function checkInput(searchInput, label) {
-    if (searchInput.value !== '') {
-      label.classList.add('active-label1');
-    } else {
-      label.classList.remove('active-label1');
-    }
-}
-
-input_country.addEventListener('input', () => {
-    checkInput(input_country, label_country);
+createLI();
+countryUl.style.display = "none";
+input_country.addEventListener("click", () => {
+    countryUl.style.display = "flex";
+    countryUl.style.justifyContent = "start";
+    countryUl.style.width = input_country.style.width;
 });
 
-form_block.addEventListener('submit', (event) => {
-    event.preventDefault();
 
-    localStorage.setItem("change", "true");
-    change = JSON.parse(localStorage.getItem("change"));
+// Filtrar por nombre la lista de paises
+let countries = document.querySelectorAll(".countryLi");
+input_country.addEventListener('input', function() {
+    const searchTerm = input_country.value.toUpperCase();
 
+    countries.forEach(countrie => {
+        const text = countrie.textContent.toUpperCase();
+        if (text.includes(searchTerm)) {
+            countrie.style.display = 'block';
+        } else {
+            countrie.style.display = 'none';
+        }
+    });
 });
 
-function changePresentation(change){
-    if(change){
-        header_section.style.flexDirection = "row";
-        header_section.style.height = "10%";
-        header_section.style.width = "100%";
-        header_section.style.justifyContent = "space-around";
-        form_block.style.flexDirection = "row";
-        form_block.style.height = "auto";
-        form_block.style.width = "35%";
-        main.style.flexDirection = "column";
-        main.style.justifyContent = "space-between";
-        main.appendChild(weather_map_section);
+document.addEventListener("click", (event) => {
+    if (!input_country.contains(event.target) && !countryUl.contains(event.target)) {
+        countryUl.style.display = "none";
     }
-}
-function createWeatherMapSection() {
+})
+// let change = JSON.parse(localStorage.getItem("change"));
+
+// changePresentation(change);
+// createWeatherMapSection();
+
+// form_block.addEventListener('submit', (event) => {
+//     event.preventDefault();
+    header_section.style.flexDirection = "row";
+    header_section.style.height = "10%";
+    header_section.style.width = "100%";
+    header_section.style.justifyContent = "space-around";
+    form_block.style.flexDirection = "row";
+    form_block.style.height = "auto";
+    form_block.style.width = "35%";
+    main.style.flexDirection = "column";
+    main.style.justifyContent = "space-between";
     weather_map_section.classList.add("weather_map_container");
     weather_container.classList.add("weather_container");
     map_container.classList.add("map_container");
     weather_map_section.appendChild(weather_container);
     weather_map_section.appendChild(map_container);
-}
+    main.appendChild(weather_map_section);
+//     localStorage.setItem("change", "true");
+//     change = JSON.parse(localStorage.getItem("change"));
+
+// });
+
+// function changePresentation(change){
+//     if(change){
+//         header_section.style.flexDirection = "row";
+//         header_section.style.height = "10%";
+//         header_section.style.width = "100%";
+//         header_section.style.justifyContent = "space-around";
+//         form_block.style.flexDirection = "row";
+//         form_block.style.height = "auto";
+//         form_block.style.width = "35%";
+//         main.style.flexDirection = "column";
+//         main.style.justifyContent = "space-between";
+//         main.appendChild(weather_map_section);
+//     }
+// }
+// function createWeatherMapSection() {
+    // weather_map_section.classList.add("weather_map_container");
+    // weather_container.classList.add("weather_container");
+    // map_container.classList.add("map_container");
+    // weather_map_section.appendChild(weather_container);
+    // weather_map_section.appendChild(map_container);
+// }
